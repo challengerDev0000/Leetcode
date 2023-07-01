80111de2a524df86327d3eacc395a05f20f5cf3e
luckyman <133821588+olivertimdev@users.noreply.github.com>
2023-07-01 01:33:10 -0700

    Time: 91 ms (19.26%), Space: 50.1 MB (66.34%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    let solution = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    nums[i] = Math.max(nums[i], nums[i] + nums[i - 1]);
+    solution = Math.max(solution, nums[i]);
+  }
+  return solution;
+};
