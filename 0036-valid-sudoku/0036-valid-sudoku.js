e56df0b95ee6c103babe95d43db6f29815c4ed57
luckyman <133821588+olivertimdev@users.noreply.github.com>
2023-07-04 22:43:17 -0700

    Time: 70 ms (84.60%), Space: 45.2 MB (64.76%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.js
@@ -0,0 +1,58 @@
+var isValidSudoku = function(board) {
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+            const value = board[i][j];
+            if (value !== '.') {
+                if (!isValidRow(board, i, j, value) || !isValidCol(board, i, j, value) | !isValidBox(board, i, j, value)) {
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+};
+
+function isValidRow(board, row, col, value) {
+    // j represents on col
+    for (let j = 0; j < 8; j++) {
+        // check first to see if the current col matches the passed in col
+        if (j !== col) {
+            if (board[row][j] === value) {
+                return false; 
+            }
+        }
+    }
+    
+    return true;
+}
+
+function isValidCol(board, row, col, value) {
+     // j represents on row
+    for (let i = 0; i < 8; i++) {
+        // check first to see if the current row matches the passed in row
+        if (i !== row) {
+            if (board[i][col] === value) {
+                return false; 
+            }
+        }
+    }
+    
+    return true;
+}
+
+function isValidBox(board, row, col, value) {
+    const startRow = row - (row % 3);
+    const startCol = col - (col % 3);
+    
+    for (let i = startRow; i < startRow + 3; i++) {
+        for (let j = startCol; j < startCol + 3; j++) {
+            if (i !== row && j !== col) {
+                if (board[i][j] === value) {
+                    return false;
+                }
+            }
+        }
+    }
+    
+    return true;
+}
