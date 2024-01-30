714c339c56123b2e567087977a87754d07d579b7
luckyman <133821588+challengerDev0000@users.noreply.github.com>
2024-01-30 17:34:08 +0200

    Time: 57 ms (82.24%), Space: 52.6 MB (11.99%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+function evalRPN(tokens) {
+  const stack = [];
+  
+  for (let token of tokens) {
+    if (token === '+') {
+      stack.push(stack.pop()+stack.pop());
+    } else if (token === '-') {
+      stack.push(-stack.pop()+stack.pop());
+    } else if (token === '*') {
+      stack.push(stack.pop()*stack.pop());
+    } else if (token === '/') {
+      stack.push(Math.trunc(1/stack.pop()*stack.pop()));
+    } else {
+      stack.push(parseInt(token));
+    }
+  }
+  
+  return stack[0];
+}
