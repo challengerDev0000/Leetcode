0868ce7a02fddd30f5773aff5f8527e62c5f3d90
luckyman <133821588+challengerDev0000@users.noreply.github.com>
2024-02-20 08:40:58 +0200

    Time: 47 ms (95.79%), Space: 51.3 MB (27.97%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    let sum = 0, total = 0
+    for(let i = 0; i < nums.length; i++) {
+        sum += nums[i]
+        total += i + 1
+    }
+    return total - sum
+};
